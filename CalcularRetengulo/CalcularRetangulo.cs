939ffee2db63ed3e2cb52bf7daf45a1d3849da38
Marcelo Siqueira Oliveira <mss.siqueira@gmail.com>
2024-03-03 16:20:51 -0300

    segundo commit com O.O.

---
+++ CalcularRetengulo/CalcularRetangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class CalculadoraRetangulo
+{
+    public static void CalcularArea()
+    {
+        Console.WriteLine("Digite a largura: ");
+        int largura = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a altura: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        double area = largura * altura;
+
+        Console.WriteLine($"A área do retângulo é: {area:F2}");
+    }
+
+    public static void CalcularPerimetro()
+    {
+        Console.WriteLine("Digite a largura: ");
+        int largura = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a altura: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        double perimetro = 2 * (largura + altura);
+
+        Console.WriteLine($"O perímetro do retângulo é: {perimetro:F2}");
+    }
+
+    public static void CalcularDiagonal()
+    {
+        Console.WriteLine("Digite a largura: ");
+        int largura = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a altura: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        double diagonal = Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+
+        Console.WriteLine($"A diagonal do retângulo é: {diagonal:F2}");
+    }
+}
--- CalcularRetengulo/Program.cs
+++ CalcularRetengulo/Program.cs
@@ -1,69 +1,32 @@
 ﻿using System;
 
-namespace CalculoRetangulo
+namespace CalcularRetangulo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escolha o cálculo:");
-            Console.WriteLine("1 - Área do Retângulo");
-            Console.WriteLine("2 - Perímetro do Retângulo");
-            Console.WriteLine("3 - Diagonal do Retângulo");
+            Console.WriteLine("Escolha o calculo:");
+            Console.WriteLine("1 - Área do Retangulo");
+            Console.WriteLine("2 - Perimetro do Retangulo");
+            Console.WriteLine("3 - Diagonal do Retangulo");
 
             int opcao = int.Parse(Console.ReadLine());
+
 
             switch (opcao)
             {
                 case 1:
-                    CalcularArea();
+                    CalculadoraRetangulo.CalcularArea();
                     break;
                 case 2:
-                    CalcularPerimetro();
+                    CalculadoraRetangulo.CalcularPerimetro();
                     break;
                 case 3:
-                    CalcularDiagonal();
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida");
+                    CalculadoraRetangulo.CalcularDiagonal();
                     break;
             }
         }
 
-        static void CalcularArea()
-        {
-            Console.WriteLine("Digite a largura: ");
-            int largura = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite a altura: ");
-            int altura = int.Parse(Console.ReadLine());
-
-            double area = largura * altura;
-
-            Console.WriteLine($"A área do retângulo é: {area:F2}");
-        }
-
-        static void CalcularPerimetro()
-        {
-            Console.WriteLine("Digite a largura: ");
-            int largura = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite a altura: ");
-            int altura = int.Parse(Console.ReadLine());
-
-            double perimetro = 2 * (largura + altura);
-
-            Console.WriteLine($"O perímetro do retângulo é: {perimetro:F2}");
-        }
-
-        static void CalcularDiagonal()
-        {
-            Console.WriteLine("Digite a largura: ");
-            int largura = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite a altura: ");
-            int altura = int.Parse(Console.ReadLine());
-
-            double diagonal = Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
-
-            Console.WriteLine($"A diagonal do retângulo é: {diagonal:F2}");
-        }
     }
 }
